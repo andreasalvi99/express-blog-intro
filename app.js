cb002d3b5257ee5d220b525fad3709a3a49d6e94
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-01 12:23:36 +0100

    add welcome message

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 const appUrl = `http://localhost:${port}`;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.json({
+    message: "Benvenuto",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server online on ${appUrl}`);
